83e1c992cab42cd6c1301127855151e7cb53160a
P-iknow <apricotsoul@gmail.com>
2019-12-29 14:37:52 +0900

    pratice: img flex

--- class-test/src/App.js
+++ class-test/src/App.js
@@ -5,26 +5,30 @@ import ClassExam from "./ClassExam";
 import FunctionExam from "./FunctionExam";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <FunctionExam />
-      <ClassExam />
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<img
+					src="https://res.tutoring.co.kr/res/images/evt/202001/05_img_care.png"
+					alt=""
+				/>
+			</header>
+			<FunctionExam />
+			<ClassExam />
+		</div>
+	);
 }
 
 export default App;
