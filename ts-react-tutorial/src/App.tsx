a0bc3c41cbe6fe95d41dab07b694f8e4d210f252
P-iknow <apricotsoul@gmail.com>
2020-01-05 01:31:58 +0900

    practice: 타입스크립트로 리엑트 Hooks 사용하기

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Counter from "./Counter";
 import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 };
 
 export default App;
