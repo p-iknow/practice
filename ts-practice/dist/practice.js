c67e57499a984be9621dd850e80f5cccd3e15c40
P-iknow <apricotsoul@gmail.com>
2020-01-04 17:23:39 +0900

    practice typescript basic

--- ts-practice/dist/practice.js
+++ ts-practice/dist/practice.js
@@ -1,14 +1,31 @@
 "use strict";
-var count = 0; // 숫자
-count += 1;
-count = "갑자기 분위기 문자열"; // 이러면 에러가 납니다!
-var message = "hello world"; // 문자열
-var done = true; // 불리언 값
-var numbers = [1, 2, 3]; // 숫자 배열
-var messages = ["hello", "world"]; // 문자열 배열
-messages.push(1); // 숫자 넣으려고 하면.. 안된다!
-var mightBeUndefined = undefined; // string 일수도 있고 undefined 일수도 있음
-var nullableNumber = null; // number 일수도 있고 null 일수도 있음
-var color = "red"; // red, orange, yellow 중 하나임
-color = "yellow";
-color = "green"; // 에러 발생!
+function sumArray(numbers) {
+    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
+}
+function returnNothing() {
+    console.log("I am just saying hello world");
+}
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수를 구현합니다.
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
