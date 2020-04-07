f932ba40bea16b686b629bd9f66a15ead58e9ec3
P-iknow <apricotsoul@gmail.com>
2020-04-07 22:51:48 +0900

    practice: Storybook을 다양한 Addon과 함께 활용해보면서 사용법 정복하기

--- storybook-tutorial/.storybook/main.js
+++ storybook-tutorial/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register', '@storybook/addon-docs/react/preset'],
 };
