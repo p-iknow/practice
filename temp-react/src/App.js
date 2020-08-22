71c35f5bc3dfc653b7b0829530c284540e1fde38
P-iknow <apricotsoul@gmail.com>
2020-08-22 09:12:32 +0900

    test: useEffect rendering

--- temp-react/src/App.js
+++ temp-react/src/App.js
@@ -3,9 +3,14 @@ import React, { useEffect, useState } from 'react';
 function App() {
 	const [count, setCount] = useState(0);
 	useEffect(() => {
+		console.log('mounted');
 		console.log(count)
-	}, [])
 
+		return () => {
+			console.log('un mounted')
+		}
+	}, [])
+	console.log("render")
 	return (
 		<div className="App">
 			<h1>useEffect test</h1>
