27c2e05ace0d877e7ade3187b76ee9f0a3569493
P-iknow <apricotsoul@gmail.com>
2020-10-01 16:22:23 +0900

    practice: flatten type

--- ts/conditional.ts
+++ ts/conditional.ts
@@ -112,3 +112,30 @@ const world: World = null as any;
 
 const dino = world.getItem(10);
 const what = world.getItem(true); // Error! Argument of type 'boolean' is not assignable to parameter of type 'string | number'.ts(2345)
+
+/*  Flatten  */
+
+type Flatten<T> = T extends any[]
+  ? T[number]
+  : T extends object
+  ? T[keyof T]
+  : T;
+
+
+const numbers = [1, 2, 3];
+type NumbersArrayFlattened = Flatten<typeof numbers>;
+// 1. number[]
+// 2. number
+
+const person = {
+  name: 'Mark',
+  age: 38
+};
+
+type SomeObjectFlattened = Flatten<typeof person>;
+// 1. keyof T --> "id" | "name"
+// 2. T["id" | "name"] --> T["id"] | T["name"] --> number | string
+
+const isMale = true;
+type SomeBooleanFlattened = Flatten<typeof isMale>;
+// true
